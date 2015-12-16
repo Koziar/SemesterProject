f7683f9e230431344fe65fb0cf666dc375f62702
Georgina Thomsen <georgina-thomsen@hotmail.com>
2015-12-16 01:49:03 +0100

    jjhjh

--- semesterSeedSP/src/java/engine/MetaEngine.java
+++ semesterSeedSP/src/java/engine/MetaEngine.java
@@ -1,85 +1,48 @@
-
 package engine;
 
-import entity.Url;
-import errorHandling.FlightException;
-import errorHandling.InternalServerException;
-import facades.FlightFacadeInterface;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+import java.util.Scanner;
 
-public class MetaEngine implements FlightFacadeInterface
-{
-    //this class handles the search for all airline flights
+/**
+ *
+ * @author wookash
+ */
+public class MetaEngine {
 
-    private List<Url> airlines;
-    private List<Flight> flights;
-    private DataProcessor dataProcessor;
+    Pinger p = new Pinger();
 
-    public MetaEngine(List<Url> airlines)
-    {
-
-        this.airlines = airlines;
-        this.flights = new ArrayList();
-        this.dataProcessor = new DataProcessor();
+    public String getFlightsFrom(String from, String date, Integer persons) throws MalformedURLException, IOException {
+        Pinger p = new Pinger();
+        URL url = new URL(p.getUrlsThreadPingerThingy() + from + "/" + date + "/" + persons);
+//     URL url =p.getUrlsThreadPingerThingy();
+//        URL url = new URL("http://angularairline-plaul.rhcloud.com/api/flightinfo/" + from + "/" + date + "/" + persons);
+        return getInfoFromGivenURL(url);
     }
 
-    //start the SkyScanner to scan for flights in each airline
-    private void startSkyScanner(FlightInfo flightInfo) throws InternalServerException, FlightException
-    {
-        List<Future<String>> futures = new ArrayList();
+    public String getFlightsFromTO(String from, String to, String date, Integer persons) throws MalformedURLException, IOException {
+        URL url = new URL(p.getUrlsThreadPingerThingy() + from + "/" + to + "/" + date + "/" + persons);
 
-        //create a thread pool which creates a number of threads equal to the number of airlines
-        ExecutorService executor = Executors.newFixedThreadPool(airlines.size());
+//        URL url = new URL("http://angularairline-plaul.rhcloud.com/api/flightinfo/" + from + "/" + to + "/" + date + "/" + persons);
+        return getInfoFromGivenURL(url);
+    }
 
-        for (Url airline : airlines) {
-            //new scanner for each airline
-            SkyScanner scanner = new SkyScanner(airline, flightInfo);
+    private String getInfoFromGivenURL(URL url) throws IOException {
 
-            //start a new Callable for each of the airlines
-            Future<String> future = executor.submit(scanner);
-            //add the future from the scanner to the futures list
-            futures.add(future);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/json; charset=UTF-8");
+        Scanner scan = new Scanner(con.getInputStream());
+        String jsonStr = "";
+        while (scan.hasNext()) {
+            jsonStr += scan.nextLine();
         }
-        executor.shutdown();
+        scan.close();
 
-        for (Future<String> future : futures) {
-            try {
-                //get the flight from the future
-                String info = future.get();
-                //pass the flights from each airline to the DataProcessor which will return them as a Java objects
-                List<Flight> flightsFromAirline = dataProcessor.getListOfFlights(info);
-                //add the flight to the flights list
-                for (Flight flight : flightsFromAirline) {
-                    flights.add(flight);
-                }
-            } catch (InterruptedException ex) {
-                //throw a custom exception from the Server
-                throw new InternalServerException("We are sorry for the inconvinience!Please try again :) ");
-            } catch (ExecutionException ex) {
-                //throw a custom exception from the Server
-                throw new InternalServerException("We are sorry for the inconvinience!Please try again :) ");
-            }
-        }
+        return jsonStr;
     }
-    @Override
-    public List<Flight> getAllFlightsFrom(FlightInfo flightInfo) throws InternalServerException, FlightException
-    {
-        //start the scanner
-        startSkyScanner(flightInfo);
-        //return the flights
-        return flights;
-    }
-    @Override
-    public List<Flight> getAllFlightsFromTo(FlightInfo flightInfo) throws InternalServerException, FlightException
-    {
-        //start the scanner
-        startSkyScanner(flightInfo);
-        //return the flights
-        return flights;
-    }
+
 }
--- semesterSeedSP/src/java/facades/UrlFacade.java
+++ semesterSeedSP/src/java/facades/UrlFacade.java
@@ -5,14 +5,21 @@
  */
 package facades;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import deploy.DeploymentConfiguration;
 import entity.Url;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.Query;
-
-
+import net.minidev.json.JSONObject;
 
 /**
  *
@@ -20,50 +27,69 @@ import javax.persistence.Query;
  */
 public class UrlFacade {
 
-    private EntityManagerFactory emf;
-
-    public UrlFacade(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
-
-    public EntityManager getEntityManager() {
-        return emf.createEntityManager();
-    }
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory(DeploymentConfiguration.PU_NAME);
+    private EntityManager em = emf.createEntityManager();
 
     public Url addUrlToDB(Url u) {
-
-        EntityManager em = emf.createEntityManager();
-
-        u = new Url("Angular Airline", "Test Airline for Sprint 1", "http://angularairline-plaul.rhcloud.com/");
-
+        List<Url> url = new ArrayList<>();
+        Url u1 = new Url("Angular Airline", "Test Airline for Sprint 1", "http://angularairline-plaul.rhcloud.com/");
+        Url u2 = new Url("Angular Airline2", "Test Airline for Sprint 2", "http://www.developerscrappad.com/228/java/java-ee/ejb3-jpa-dealing-with-date-time-and-timestamp/");
+        url.add(u1);
+        url.add(u2);
         try {
             em.getTransaction().begin();
-//        for (Url url : urls) {
-//            em.persist(url);
-//        }
-            em.persist(u);
+            for (Url url1 : url) {
+                em.persist(url1);
+            }
+
+//            em.persist(u);
             em.getTransaction().commit();
             return u;
         } finally {
-            em.close();
+//            em.close();
         }
+
     }
 
     public List<Url> getUrls() {
-
-//        EntityManager em = emf.createEntityManager();
-//
-//        Query query = em.createQuery("SELECT u FROM Url u", Url.class
-//        );
-//
-//        List<Url> airlines = query.getResultList();
-//        return query.getResultList();
-        
-        //Temporary added airlines
-        List<Url> testFlight = new ArrayList();
-        testFlight.add(new Url("test", "", "http://angularairline-plaul.rhcloud.com/api"));
-        testFlight.add(new Url("outsourcing", "", "http://sargardon-001-site1.atempurl.com/api"));
-        
-        return testFlight;
+        Query query = em.createQuery("SELECT u FROM Url u", Url.class);
+        return query.getResultList();
     }
+//    public static void main(String[] args) {
+//        Url u1 = new Url("hhhh", "jjjjjj", "kkkkkkkk");
+//        UrlFacade u = new UrlFacade();
+//        u.addUrlToDB(new Url("Angular Airline", "Test Airline for Sprint 1", "http://angularairline-plaul.rhcloud.com/"));
+//        String j1 = getJSONFromPerson(u1);
+//        System.out.println();
+//    }
+
+//    private static String readUrl(String urlString) throws Exception {
+//        BufferedReader reader = null;
+//        try {
+//            URL url = new URL(urlString);
+//            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+//            StringBuffer buffer = new StringBuffer();
+//            int read;
+//            char[] chars = new char[1024];
+//            while ((read = reader.read(chars)) != -1) {
+//                buffer.append(chars, 0, read);
+//            }
+//
+//            return buffer.toString();
+//        } finally {
+//            if (reader != null) {
+//                reader.close();
+//            }
+//        }
+//    }
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create();
+//
+
+//    public static void main(String[] args) {
+//        UrlFacade uf = new UrlFacade();
+//        uf.addUrlToDB(new Url("Angular Airline", "Test Airline for Sprint 1", "http://angularairline-plaul.rhcloud.com/"));
+//
+//        uf.getUrls();
+//        System.out.println(uf.getUrls());
+//    }
 }
